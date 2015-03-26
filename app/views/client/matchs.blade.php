e5b37ecbe814fbc89e0374a5530da5244f61caa9
Michael <hungnv038@gmail.com>
2015-03-26 23:38:31 +0700

    format data display

--- app/views/client/matchs.blade.php
+++ app/views/client/matchs.blade.php
@@ -21,7 +21,7 @@
             <?php
                 $start_time=DateTime::createFromFormat('Y-m-j H:i:s',$match->time_1)->format("M-j H:i");
                     $minute="";
-                    $time_color=0x000;
+                    $time_color="";
                     $ht_score='';
                     $score='<font color="blue"> - </font>';
                 if(intval($match->status)==-1) {
@@ -39,14 +39,14 @@
                     $minute=floor((time()-strtotime($match->time_2))/60);
                     if($minute<1) $minute=1;
                     if($minute>45) $minute="45+";
-                    $time_color='<font color="0x0066ff">'.$minute.'</font>';
+                    $time_color='<font color="0x0066ff">'.$minute.'</font><image src="http://www.nowgoal.com/images/in.gif" border="0"/>';
 
                     $score='<font color="blue">'.$match->h_goal.' - '.$match->g_goal.'</font>';
                 }elseif(intval($match->status)==3) {
                     $minute=floor(46+(time()-strtotime($match->time_2))/60);
                     if($minute<46) $minute=46;
                     if($minute>90) $minute="90+";
-                    $time_color='<font color="0x0066ff">'.$minute.'</font>';
+                    $time_color='<font color="0x0066ff">'.$minute.'</font><image src="http://www.nowgoal.com/images/in.gif" border="0"/>';
 
                     $ht_score='<font color="red">'.$match->ht_h_goal.' - '.$match->ht_g_goal.'</font>';
 
@@ -56,6 +56,7 @@
             <tr>
                 <td style="background-color: {{$match->color}}}; color: #ffffff; text-align: center">
                     {{$match->code}}
+                    <image src="http://www.nowgoal.com/images/in.gif" border="0"/>
                 </td>
                 <td style="text-align: center">{{DateTime::createFromFormat('Y-m-j H:i:s',$match->time_1)->format("M-j H:i")}}</td>
                 <td style="text-align: center">
