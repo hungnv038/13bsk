da93f29692e5ed912469f1ed823847dd27099564
Michael <hungnv038@gmail.com>
2015-04-03 21:15:26 +0700

    fixed

--- app/controllers/OddController.php
+++ app/controllers/OddController.php
@@ -252,7 +252,7 @@ class OddController extends BaseController{
                 (rules.type<=2 and rules.after_odd = odds.draw) or
                 (rules.type=3 and rules.after_odd +odds.score1+odds.score2= odds.draw) OR
                 (rules.type=4 and abs(rules.after_odd) < abs(odds.draw) and abs(odds.draw) !=999) or
-                (rules.type=5 and rules.after_odd < odds.draw)
+                (rules.type=5 and rules.after_odd +odds.score1+odds.score2 < odds.draw)
                 where  time in (45,-2) and odds.type=rules.data_type
                 group by match_id, rule_id
                 order by time desc";
